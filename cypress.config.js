310d9f4c2a2f8861172deb976b30783fc788d57c
vladasko-g <vladasko@protonmail.com>
2024-02-11 10:04:43 +0200

    e

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:3003",
-    specPattern: "cypress/e2e",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     viewportHeight: 900,
     viewportWidth: 600,
   },
